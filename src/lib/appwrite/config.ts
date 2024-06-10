2924822bbcade919e673fc6424e61e110efd63cb
Ashish Sharma <Ashish121216@gmail.com>
2024-06-10 11:20:43 +0530

    updated client

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -13,7 +13,7 @@ export const appwriteConfig = {
 export const client = new Client();
 
 client.setProject(appwriteConfig.projectId);
-client.setEndpoint('https://cloud.appwrite.io/v1');
+client.setEndpoint('https://socialkid123.netlify.app/');
 
 export const account = new Account(client);
 export const databases = new Databases(client);
